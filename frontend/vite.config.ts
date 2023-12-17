07c84821a2a572aeef3ba90fefca25e18ea38ebf
BartoszBugla <48729791+BartoszBugla@users.noreply.github.com>
2023-12-17 17:42:50 +0100

    Deploy to gihtub pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,9 @@
 // vite.config.ts
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: '/cycly',
   plugins: [tsconfigPaths(), react()],
 });
