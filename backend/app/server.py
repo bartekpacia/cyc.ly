2d393ff881322a38810330579d73029d7f4fc263
Bartek Pacia <barpac02@gmail.com>
2024-01-18 14:22:34 +0100

    remove database

--- backend/app/server.py
+++ backend/app/server.py
@@ -8,30 +8,6 @@ from routes.routes_router import routes_router
 def init_routers(app_: FastAPI) -> None:
     app_.include_router(routes_router)
 
-
-# def init_listeners(app_: FastAPI) -> None:
-#     # Exception handler
-#     @app_.exception_handler(CustomException)
-#     async def custom_exception_handler(request: Request, exc: CustomException):
-#         return JSONResponse(
-#             status_code=exc.code,
-#             content={"error_code": exc.error_code, "message": exc.message},
-#         )
-
-
-# def on_auth_error(request: Request, exc: Exception):
-#     status_code, error_code, message = 401, None, str(exc)
-#     if isinstance(exc, CustomException):
-#         status_code = int(exc.code)
-#         error_code = exc.error_code
-#         message = exc.message
-
-#     return JSONResponse(
-#         status_code=status_code,
-#         content={"error_code": error_code, "message": message},
-#     )
-
-
 def make_middleware() -> list[Middleware]:
     middleware = [
         Middleware(
@@ -41,13 +17,6 @@ def make_middleware() -> list[Middleware]:
             allow_methods=["*"],
             allow_headers=["*"],
         ),
-        # Middleware(
-        #     AuthenticationMiddleware,
-        #     backend=AuthBackend(),
-        #     on_error=on_auth_error,
-        # ),
-        # Middleware(SQLAlchemyMiddleware),
-        # Middleware(ResponseLogMiddleware),
     ]
     return middleware
 
@@ -59,7 +28,6 @@ def create_app() -> FastAPI:
         version="1.0.0",
         docs_url="/docs",
         redoc_url="/redoc",
-        # dependencies=[Depends(Logging)],
         middleware=make_middleware(),
     )
     init_routers(app_=app_)
--- backend/sql-scripts/2-seed.sql
+++
@@ -1,4 +0,0 @@
-USE cycly;
-
-INSERT INTO users (email)
-VALUES ('tester1@cycly.com'), ('tester2@cycly.com');
